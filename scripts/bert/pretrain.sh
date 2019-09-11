709cc75e8cc724cd303690cd93b5ab96e2a255be
Ubuntu <ubuntu@ip-172-31-9-230.us-west-2.compute.internal>
2019-09-11 17:55:43 +0000

    add pretrain script

---
+++ scripts/bert/pretrain.sh
@@ -0,0 +1,9 @@
+MXNET_SAFE_ACCUMULATION=1 \
+python run_pretraining.py \
+	--gpus='0,1,2,3,4,5,6,7' \
+	--model='bert_24_1024_16' \
+	--data='/home/ubuntu/mxnet-data/bert-pretraining/datasets/book-corpus/book-corpus-large-split/*.train,/home/ubuntu/mxnet-data/bert-pretraining/datasets/enwiki/enwiki-feb-doc-split/*.train' \
+	--data_eval='/home/ubuntu/mxnet-data/bert-pretraining/datasets/book-corpus/book-corpus-large-split/*.dev,/home/ubuntu/mxnet-data/bert-pretraining/datasets/enwiki/enwiki-feb-doc-split/*.dev' \
+	--raw --num_steps 900000 --max_seq_length 128 --max_predictions_per_seq 20 --short_seq_prob 1.0 \
+	--lr 1e-4  --warmup_ratio 0.01 --batch_size 32 \
+       	--log_interval 100 --ckpt_dir ./baseline_ckpt_dir --ckpt_interval 1000 --verbose 2>&1
