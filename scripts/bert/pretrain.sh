467dda8cc5828f6d6be2394d2bffb370f60177d6
Lin Yuan <apeforest@gmail.com>
2019-09-11 18:07:10 +0000

    remove verbose

--- scripts/bert/pretrain.sh
+++ scripts/bert/pretrain.sh
@@ -5,5 +5,4 @@ python run_pretraining.py \
 	--data='/home/ubuntu/mxnet-data/bert-pretraining/datasets/book-corpus/book-corpus-large-split/*.train,/home/ubuntu/mxnet-data/bert-pretraining/datasets/enwiki/enwiki-feb-doc-split/*.train' \
 	--data_eval='/home/ubuntu/mxnet-data/bert-pretraining/datasets/book-corpus/book-corpus-large-split/*.dev,/home/ubuntu/mxnet-data/bert-pretraining/datasets/enwiki/enwiki-feb-doc-split/*.dev' \
 	--raw --num_steps 900000 --max_seq_length 128 --max_predictions_per_seq 20 --short_seq_prob 1.0 \
-	--lr 1e-4  --warmup_ratio 0.01 --batch_size 32 \
-       	--log_interval 100 --ckpt_dir ./baseline_ckpt_dir --ckpt_interval 1000 --verbose 2>&1
+	--lr 1e-4  --warmup_ratio 0.01 --batch_size 32 --batch_size_eval 32 --ckpt_dir ./baseline_ckpt_dir 2>&1
